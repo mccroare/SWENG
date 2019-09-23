7be4d3ef4b4050c811f8eb8c9a02aa7c20db5e15
Evan <mccroare@tcd.ie>
2019-09-23 01:46:07 +0100

    Initial test file

---
+++ LowestCommonAncestorTest.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * 
+ */
+
+/**
+ * @author Evan McCroary
+ *
+ */
+class LowestCommonAncestorTest {
+
+	@Test
+	void test() {
+	}
+	
+}
+
