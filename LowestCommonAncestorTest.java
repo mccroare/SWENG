c06001d64336e136f964e1e60d80f1835995b333
Evan <mccroare@tcd.ie>
2019-10-10 16:48:46 +0100

    Tests LCA is not root and LCA is root

--- LowestCommonAncestorTest.java
+++ LowestCommonAncestorTest.java
@@ -12,9 +12,36 @@ import static org.junit.Assert.assertEquals;
  */
 class LowestCommonAncestorTest {
 
+    // LCA is not root
 	@Test
-	void test() {
+	void testNotRoot() {
+		LowestCommonAncestor tree = new LowestCommonAncestor(); 
+        tree.root = new Node(1); 
+        tree.root.left = new Node(2); 
+        tree.root.right = new Node(3); 
+        tree.root.left.left = new Node(4); 
+        tree.root.left.right = new Node(5); 
+        tree.root.right.left = new Node(6); 
+        tree.root.right.right = new Node(7); 
+        
+		assertEquals(tree.findLCA(4,5), 2);
 	}
+	
+	// LCA is root
+	@Test
+	void testIsRoot() {
+		LowestCommonAncestor tree = new LowestCommonAncestor(); 
+        tree.root = new Node(1); 
+        tree.root.left = new Node(2); 
+        tree.root.right = new Node(3); 
+        tree.root.left.left = new Node(4); 
+        tree.root.left.right = new Node(5); 
+        tree.root.right.left = new Node(6); 
+        tree.root.right.right = new Node(7); 
+        
+		assertEquals(tree.findLCA(4,6), 1);
+	}
+	
 	
 }
 
