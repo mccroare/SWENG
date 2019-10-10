183c05e31a2bc939e1be5296436b5c72d5375439
Evan <mccroare@tcd.ie>
2019-10-10 17:05:46 +0100

    Test for LCA is node 1 and LCA is node 2

--- LowestCommonAncestorTest.java
+++ LowestCommonAncestorTest.java
@@ -41,6 +41,36 @@ class LowestCommonAncestorTest {
         
 		assertEquals(tree.findLCA(4,6), 1);
 	}
+
+	// LCA is node1
+	@Test
+	void testIsNode1() {
+		LowestCommonAncestor tree = new LowestCommonAncestor(); 
+	tree.root = new Node(1); 
+	tree.root.left = new Node(2); 
+	tree.root.right = new Node(3); 
+	tree.root.left.left = new Node(4); 
+	tree.root.left.right = new Node(5); 
+	tree.root.right.left = new Node(6); 
+	tree.root.right.right = new Node(7); 
+	        
+		assertEquals(tree.findLCA(3,7), 3);
+	}
+		
+	// LCA is node2
+	@Test
+	void testIsNode2() {
+		LowestCommonAncestor tree = new LowestCommonAncestor(); 
+	tree.root = new Node(1); 
+	tree.root.left = new Node(2); 
+	tree.root.right = new Node(3); 
+	tree.root.left.left = new Node(4); 
+	tree.root.left.right = new Node(5); 
+	tree.root.right.left = new Node(6); 
+	tree.root.right.right = new Node(7); 
+	        
+		assertEquals(tree.findLCA(6,3), 3);
+	}
 	
 	
 }
